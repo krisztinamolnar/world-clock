beca3a79415084bf955610a43929b242fccffa81
krisztinamolnar <krisztina.molnar.mail@gmail.com>
2024-02-25 15:40:00 +0100

    Added date and time for two locations

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,20 @@
+function updateTime() {
+  //Rome
+  let romeElement = document.querySelector("#rome");
+  let romeDateElement = romeElement.querySelector(".date");
+  let romeTimeElement = romeElement.querySelector(".time");
+  let romeTime = moment().tz("Europe/Rome");
+  romeDateElement.innerHTML = romeTime.format("MMMM Do YYYY");
+  romeTimeElement.innerHTML = romeTime.format("h:mm:ss [<small>]A[</small>]");
 
+  //Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
