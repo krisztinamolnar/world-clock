a74ba453f3b790947ebf12980e00c77b15638872
krisztinamolnar <krisztina.molnar.mail@gmail.com>
2024-02-25 20:17:48 +0100

    Modified the displayed cities and select options

--- javascript/index.js
+++ javascript/index.js
@@ -9,6 +9,16 @@ function updateTime() {
     romeTimeElement.innerHTML = romeTime.format("HH:mm:ss");
   }
 
+  //Johannesburg
+  let johannesburgElement = document.querySelector("#johannesburg");
+  if (johannesburgElement) {
+    let johannesburgDateElement = johannesburgElement.querySelector(".date");
+    let johannesburgTimeElement = johannesburgElement.querySelector(".time");
+    let johannesburgTime = moment().tz("Africa/Johannesburg");
+    johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
+    johannesburgTimeElement.innerHTML = johannesburgTime.format("HH:mm:ss");
+  }
+
   //Tokyo
   let tokyoElement = document.querySelector("#tokyo");
   if (tokyoElement) {
